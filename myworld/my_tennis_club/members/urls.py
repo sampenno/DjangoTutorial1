5f7c5ee870d04cce59ca5dc8c802c540ea31f371
sampenno <48610839+sampenno@users.noreply.github.com>
2025-11-30 23:48:23 +0000

    adding an image

--- myworld/my_tennis_club/members/urls.py
+++ myworld/my_tennis_club/members/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('members/', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
     path('testing/', views.testing, name='testing'),    
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
