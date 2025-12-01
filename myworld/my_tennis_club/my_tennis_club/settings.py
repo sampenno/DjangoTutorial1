6652ce5d420a7591500b64be5d110749880e75dd
sampenno <48610839+sampenno@users.noreply.github.com>
2025-12-01 02:26:15 +0000

    Corrections to Static URLS

--- myworld/my_tennis_club/my_tennis_club/settings.py
+++ myworld/my_tennis_club/my_tennis_club/settings.py
@@ -115,11 +115,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-STATIC_URL = "/static/"
-MEDIA_ROOT ="/media/"
+STATIC_URL = '/static/'
+MEDIA_ROOT = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-STATICFILES_DIR=(
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
 
